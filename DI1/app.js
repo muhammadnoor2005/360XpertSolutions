8b78941e90838fb62202f2ba70bb7c8a685bbbc9
muhammadnoor2005 <125201339+muhammadnoor2005@users.noreply.github.com>
2025-07-13 22:08:50 +0500

    Add files via upload
    updated app

--- DI1/app.js
+++ DI1/app.js
@@ -1,11 +1,13 @@
 const fastify = require("fastify");
-const di = require("./di/container");
+const diContainer = require("./di/container");
 const userRoutes = require("./routes/user.route");
 
 const server = () => {    
     const app = fastify({logger: true});
 
-    di(app);
+    // Manual Dependency Injection container
+    // now anywhere in our app we can inject dependencies through fastify.diContainer
+    app.decorate("diContainer", diContainer);
 
     app.register(userRoutes);
 
